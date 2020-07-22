83d533d8f8c805a2a9ec6ba6ae9f67dddbb6610c
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-22 18:23:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,9 +1,14 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
+  def happy_birthday(birthday_kids)
+    birthday_kids.each do |kids_name, age|
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+  end
   # add your code snippet here!
 end
